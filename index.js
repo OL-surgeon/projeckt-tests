f07d8dd4a9593bb613c9ffe1222ffc8455a38459
OL-surgeon <OL-surgeon@users.noreply.github.com>
2025-06-09 16:41:37 +0000

    deploy: e0e1df22acf0d7c690021ba916b70f94061cdb18

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))o(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&o(n)}).observe(document,{childList:!0,subtree:!0});function i(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerPolicy&&(r.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?r.credentials="include":t.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function o(t){if(t.ep)return;t.ep=!0;const r=i(t);fetch(t.href,r)}})();async function c(e=1,s=12){const i=`https://sound-wave.b.goit.study/api/artists?page=${e}&limit=${s}`,o=await fetch(i);if(!o.ok)throw new Error(`HTTP error! status: ${o.status}`);return await o.json()}function u(e){return`
-    <li class="artist-card">
-      <img src="${e.image}" alt="${e.name}" class="artist-image" />
-      <h3 class="artist-name">${e.name}</h3>
-      <p class="artist-genre">${e.genre}</p>
-    </li>
-  `}const a=document.querySelector(".artists-list");async function l(e=1,s=12){try{const i=await c(e,s),{artists:o}=i;if(!Array.isArray(o))throw new Error("Невірна структура даних: artists не є масивом");d(o)}catch(i){console.error("Помилка при завантаженні артистів:",i.message)}}function d(e){a.innerHTML="";const s=e.map(u).join("");a.insertAdjacentHTML("beforeend",s)}l();
+import{a}from"./assets/vendor-B2YOV0tR.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const r of document.querySelectorAll('link[rel="modulepreload"]'))o(r);new MutationObserver(r=>{for(const t of r)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&o(i)}).observe(document,{childList:!0,subtree:!0});function n(r){const t={};return r.integrity&&(t.integrity=r.integrity),r.referrerPolicy&&(t.referrerPolicy=r.referrerPolicy),r.crossOrigin==="use-credentials"?t.credentials="include":r.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(r){if(r.ep)return;r.ep=!0;const t=n(r);fetch(r.href,t)}})();async function l(e=1,s=12){const n=`https://sound-wave.b.goit.study/api/artists?page=${e}&limit=${s}`;try{const{data:o}=await a.get(n);return o}catch(o){return console.error("Помилка отримання артистів:",o),null}}function u(e){return!e||!e.image||!e.name||!e.genre?"":`
+        <li class="artist-card">
+            <img src="${e.image}" alt="${e.name}" class="artist-image" />
+            <h3 class="artist-name">${e.name}</h3>
+            <p class="artist-genre">${e.genre}</p>
+        </li>
+    `}const c=document.querySelector(".artists-list");async function f(e=1,s=12){try{const n=await l(e,s),{artists:o}=n;if(!Array.isArray(o))throw new Error("Невірна структура даних: artists не є масивом");d(o)}catch(n){console.error("Помилка при завантаженні артистів:",n.message)}}function d(e){c.innerHTML="";const s=e.map(u).join("");c.insertAdjacentHTML("beforeend",s)}f();
 //# sourceMappingURL=index.js.map
